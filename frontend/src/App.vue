f30e5bfa4295409a04b0c6f6b38623593686c2cb
spno77 <pilonoti@yahoo.com>
2023-02-23 16:57:37 +0200

    Initial frontend project with Vuetify

---
+++ frontend/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <Header />
+  <router-view />
+</template>
+
+<script setup>
+  import Header from '@/components/Header.vue'
+</script>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-toolbar density="compact" color="cyan">      
+
+</v-toolbar>
+</template>
+
+<script>
+export default{
+  name: 'Header'
+}
+</script>
