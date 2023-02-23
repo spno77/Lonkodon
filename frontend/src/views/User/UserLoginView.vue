221157392e1346d8145a1c9fedaa2588b1d9b89e
spno77 <pilonoti@yahoo.com>
2023-02-23 17:38:46 +0200

    Completed user Register and Login Views

--- frontend/src/views/User/UserLoginView.vue
+++ frontend/src/views/User/UserLoginView.vue
@@ -1,3 +1,48 @@
 <template>
-    <h1>user login </h1>
+    
+    <h1 class="text-md-center mt-10"> Login </h1>
+    
+    <v-sheet width="400" class="mx-auto">
+      
+      <v-form class="mt-10">
+        <v-text-field
+          v-model="user.username"
+          label="Username"
+        ></v-text-field>
+  
+        <v-text-field
+          type="password"
+          v-model="user.password"
+          label="Password"
+        ></v-text-field>
+      </v-form>
+    </v-sheet>
+
+    <v-btn color="cyan"   rounded="lg"
+         :style="{left: '50%', transform:'translateX(-50%)'}"> 
+                    Login
+    </v-btn>
+
 </template>
+  
+  <script>
+    export default {
+     data(){
+      return{
+          user:{
+              username: '',
+              password: '',
+              is_staff: null
+          },
+      }
+     }
+    }
+  </script>
+
+<style scoped>
+
+h1{
+    color: rgb(42, 137, 137); 
+   }
+
+</style>
--- frontend/src/views/User/UserRegisterView.vue
+++ frontend/src/views/User/UserRegisterView.vue
@@ -1,3 +1,116 @@
 <template>
-    <h1> user register </h1> 
-</template>
+    <h1 class="text-md-center mt-5"> Registration </h1>
+   
+    <v-container class="mt-3" >
+        <v-form>
+            
+            <v-row no-gutters >
+                <v-col class="ma-2 pa-1">
+                     <v-text-field color="cyan"
+                    v-model="user.username"
+                    label="Username"
+                    ></v-text-field>
+                </v-col>
+ 
+                <v-col class="ma-2 pa-1">
+                    <v-text-field color="cyan"
+                    v-model="user.email"
+                    label="Email"
+                    ></v-text-field>     
+                </v-col>
+            </v-row>
+ 
+            <v-row no-gutters >
+                <v-col class="ma-2 pa-1">
+                    <v-text-field color="cyan"
+                    v-model="user.firstname"
+                    label="Firstname"
+                    ></v-text-field>
+                </v-col>
+ 
+                <v-col class="ma-2 pa-1">
+                    <v-text-field color="cyan"
+                    v-model="user.lastname"
+                    label="Lastname"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+ 
+            <v-row no-gutters >
+                <v-col class="ma-2 pa-1">
+                    <v-text-field color="cyan"
+                    v-model="user.phone"
+                    label="Phone"
+                    ></v-text-field>
+                </v-col>
+ 
+                <v-col class="ma-2 pa-1">
+                    <v-text-field color="cyan"
+                    v-model="user.employment"
+                    label="Employment"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+ 
+
+                <v-text-field
+                    v-model="user.position"
+                    label="Position"
+                ></v-text-field>
+ 
+            <v-row no-gutters >
+                <v-col class="ma-2 pa-1">
+                    <v-text-field color="cyan"
+                    type="password"
+                    v-model="user.password1"
+                    label="Password"
+                    ></v-text-field>
+                </v-col>
+ 
+                <v-col class="ma-2 pa-1">
+                    <v-text-field color="cyan"
+                    type="password"
+                    v-model="user.password2"
+                    label="Re-enter password"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+        </v-form>
+ 
+    <v-btn color="cyan" rounded="lg"
+         :style="{left: '50%', transform:'translateX(-50%)'}"> 
+                    Register
+    </v-btn>
+     
+    </v-container>
+    
+ </template>
+ 
+ <script>
+   export default {
+    data(){
+     return{
+         user:{
+             username:   '',
+             email:      '',
+             password1:  '',
+             password2:  '',
+             firstname:  '',
+             lastname:   '',
+             phone:      '',
+             //image:      '',
+             employment: '',
+             position:   '',
+         },
+     }
+    }
+   }
+ </script>
+ 
+ <style scoped>
+ 
+ h1{
+     color: rgb(42, 137, 137); 
+    }
+ 
+ </style>
