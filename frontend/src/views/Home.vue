f14d6fcd42519bdef644b0e64ff3051bd5d3fc09
spno77 <pilonoti@yahoo.com>
2023-03-05 16:26:12 +0200

    Implement home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,29 @@
 <template>
+    <v-row justify="center" class="mt-16">
+        <h1> 
+            L<span style="color: blue">o</span>nk<span style="color: blue">o</span>d<span style="color: blue">o</span>n
+        </h1>
+    </v-row>
 
+    <v-row justify="center" class="mt-6">
+        <h2> 
+            Connect,Find and Post Jobs,Read Articles ...
+        </h2>
+    </v-row>
 </template>
 
 <script setup>
 
 </script>
+
+<style scoped>
+
+h1{
+    font-size: 100px;
+}
+
+h2{
+    color: rgb(25, 142, 142);
+}
+
+</style>
